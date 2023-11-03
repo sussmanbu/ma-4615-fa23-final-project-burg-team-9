5952fc7a82bcbe4f12a9bcdd89910005a97a1405
Holly Gustavsen <hgust@bu.edu>
2023-11-03 09:44:50 -0400

    added columns to data

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -1,10 +1,25 @@
-library(tidyverse)
+library(tidyverse) 
 
-loan_data <- read_csv(here::here("dataset", "loan_refusal.csv"))
+traffic_data <- read_csv(here::here("dataset-ignore", "Traffic_Violations_20231025.csv")) 
 
 ## CLEAN the data
-loan_data_clean <- loan_data
+traffic_data_clean <- traffic_data |>   
+  select("Date Of Stop", 
+         "Time Of Stop", 
+         Description, 
+         Location, 
+         Accident, 
+         Fatal, 
+         Alcohol, 
+         State, 
+         Year, 
+         Make, 
+         Model, 
+         "Violation Type", 
+         Race, 
+         Gender, 
+         "Arrest Type") 
 
-write_csv(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.csv"))
+write_csv(traffic_data_clean, file = here::here("dataset-ignore", "traffic_data_clean.csv")) 
 
-save(loan_data_clean, file = here::here("dataset/loan_refusal.RData"))
+save(traffic_data_clean, file = here::here("dataset/traffic_violations.RData"))
