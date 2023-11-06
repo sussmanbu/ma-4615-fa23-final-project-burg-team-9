94b0e39d2b78528308de5675fdf148aa4d5b31d2
Holly Gustavsen <hgust@bu.edu>
2023-11-06 09:42:21 -0500

    made blog post 3

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -1,6 +1,6 @@
 library(tidyverse) 
 
-traffic_data <- read_csv(here::here("dataset-ignore", "Traffic_Violations_20231025.csv")) 
+traffic_data <- read_csv(here::here("dataset-ignore", "Traffic_Violations_20231103.csv")) 
 
 ## CLEAN the data
 traffic_data_clean <- traffic_data |>   
@@ -11,15 +11,16 @@ traffic_data_clean <- traffic_data |>
          Accident, 
          Fatal, 
          Alcohol, 
-         State, 
          Year, 
          Make, 
          Model, 
          "Violation Type", 
          Race, 
          Gender, 
+         "DL State",
          "Arrest Type") 
 
 write_csv(traffic_data_clean, file = here::here("dataset-ignore", "traffic_data_clean.csv")) 
 
 save(traffic_data_clean, file = here::here("dataset/traffic_violations.RData"))
+
